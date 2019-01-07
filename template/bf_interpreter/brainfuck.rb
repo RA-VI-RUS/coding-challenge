0d96a8e75131d8f4484c9f093e42820baca00614
SCA2 <tpryan_1965@yahoo.com>
2019-01-06 17:23:19 -0800

    Add brainfuck and minitest tests

--- template/bf_interpreter/brainfuck.rb
+++ template/bf_interpreter/brainfuck.rb
@@ -1,9 +1,64 @@
 require_relative './environment'
+require 'io/console'
+require 'stringio'
+require 'strscan'
 
 class Brainfuck
-  def initialize(input:, output:)
+  def initialize(input: $stdin, output: $stdout)
+    $stdin = input
+    $stdout = output
   end
 
   def interpret!(script)
+    buffer = StringScanner.new(script)
+    tokens = []
+    stack = []
+    data = [0] * 30000
+    iPtr = 0
+    dPtr = 0
+    left_count = 0
+
+    while !buffer.eos? do
+      token = buffer.scan_until(/\+|-|>|<|\[|\]|\.|,/)
+      left_count += 1 if token === '['
+      left_count -= 1 if token === ']'
+      if left_count < 0 
+        $stderr.puts "Error - Missing [ bracket"
+        return
+      end
+      tokens.push(token)
+    end
+
+    if left_count > 0 
+      $stderr.puts "Error - Missing ] bracket"
+      return
+    end
+
+    while iPtr < tokens.length do
+      case tokens[iPtr]
+      when '+'
+        data[dPtr] += 1
+      when '-'
+        data[dPtr] -= 1
+      when '>'
+        dPtr += 1
+      when '<'
+        dPtr -= 1
+      when '.'
+        $stdout.print data[dPtr].chr
+      when ','
+        data[dPtr] = $stdin.getch()
+      when '['
+        stack.push(iPtr)
+      when ']'
+        iPtr = stack[-1] if data[dPtr] != 0
+        stack.pop() if data[dPtr] == 0
+      end
+
+      iPtr += 1
+    end
+  ensure
+    $stdin = STDIN
+    $stdout = STDOUT
   end
 end
--- template/bf_interpreter/test_brainfuck.rb
+++ template/bf_interpreter/test_brainfuck.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require 'minitest/autorun'
 
 require_relative './environment'
 require_relative './brainfuck'
@@ -20,5 +21,49 @@ class TestBrainfuck < Test::Unit::TestCase
   def assert_script_output(script, output)
     @interpreter.interpret!(script)
     assert_equal(output, @out.string)
+  end
+end
+
+
+describe Brainfuck do
+  before do
+    @out = StringIO.new
+    @interpreter = Brainfuck.new(input: $stdin, output: @out)
+  end
+
+  it 'parses all commands and reads a character from $stdin' do
+    script = '+-><[],.'
+    assert_output('A') do
+      $stdin = StringIO.new
+      $stdin.puts 'A'
+      $stdin.rewind
+      @interpreter.interpret!(script)
+      $stdin = STDIN
+    end
+  end
+
+  it 'generates left bracket error' do
+    script = ']'
+    assert_output(nil, /Error - Missing \[ bracket/) do
+      @interpreter.interpret!(script)
+    end
+  end
+
+  it 'generates right bracket error' do
+    script = '['
+    assert_output(nil, /Error - Missing \] bracket/) do
+      @interpreter.interpret!(script)
+    end
+  end
+
+  it 'calculates 2 + 5 and converts result to ASCII' do
+    script = '++>+++++[<+>-]++++++++[<++++++>-]<.'
+    assert_output('7') { @interpreter.interpret!(script) }
+  end
+
+  it 'prints Hello World!' do
+    script = '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.'
+    @interpreter.interpret!(script)
+    assert_output(/\AHello World!\n\z/) { @interpreter.interpret!(script) }
   end
 end
